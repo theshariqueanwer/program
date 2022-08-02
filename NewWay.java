45007ff662f1e90906df5e2ca8144f319670e781
theshariqueanwer <mdshariqueanwer01@gmail.com>
2022-08-03 00:14:20 +0530

    push to dev

---
+++ NewWay.java
@@ -0,0 +1,17 @@
+public class HelloWorld 
+{
+	public static void main(String[] args) 
+	{
+		int a = 10;
+    		int b = 0;
+    		int c = a / b;
+    		try 
+		{
+			System.out.print(c);
+		} 
+		catch(EXception ex) 
+		{
+			System.out.print(ex);
+		}
+	}
+}
